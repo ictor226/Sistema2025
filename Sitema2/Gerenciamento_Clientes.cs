3e4399a27c61f7d8c195f65bebda17a948e57a14
Victor Huggo <huggoV226@gmail.com>
2025-02-10 11:03:20 -0300

    bvc

---
+++ Sitema2/Gerenciamento_Clientes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Sitema2
+{
+    public partial class Gerenciamento_Clientes : Form
+    {
+        public Gerenciamento_Clientes()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPesquisarClientes_Click(object sender, EventArgs e)
+        {
+
+            //Defina sua string de conexão com o banco
+            string conectionString = "Server=localhost; Port=3306; Database=bd_sistema; Uid=root; Pwd=;";
+
+            try
+            {
+
+                using (MySqlConnection consulta = new MySqlConnection(conectionString))
+                {
+
+                    consulta.Open();
+                    string listagem = "SELECT Id_Cliente , NomeCompleto, Telefone FROM tb_clientes";
+
+                    using (MySqlCommand cmd = new MySqlCommand(listagem, consulta))
+                    {
+                        MySqlDataReader reader = cmd.ExecuteReader();
+
+                        DataTable dadosClientes = new DataTable();
+                        dadosClientes.Load(reader);
+
+                        dgvClientes.DataSource = dadosClientes;
+                    }
+
+
+
+                }
+
+
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Erro ao listar os clientes " + ex.Message);
+            }
+            
+            }
+        }
+    }
+
+
